a0ff8c7c648c1ef87d79a7579366d6782bbd338f
Remilia <remiliarosalia@gmail.com>
2026-03-17 15:23:51 -0400

    Fixed Scroll bar moving nav

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,13 @@ setup()
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rakkas&display=swap');
+
+
+html {
+  overflow-x: hidden;
+  margin-right: calc(-1 * (100vw - 100%));
+}
+
 #app 
 {
   font-family: Poppins,Avenir, Helvetica, Arial, sans-serif;
